97e3c56d5272a7d3fa0809349488309f305d8232
kfurkanbek <kfurkanbek@gmail.com>
2024-07-07 21:53:28 +0200

    Add a solution for Will You Make problem

---
+++ src/will_you_make.c
@@ -0,0 +1,15 @@
+#include "will_you_make.h"
+
+/**
+ * @brief A function that tells you
+if it is possible to get to the pump or not.
+ *
+ * @param distance_to_pump
+ * @param mpg
+ * @param fuel_left
+ * @return true
+ * @return false
+ */
+bool zero_fuel(double distance_to_pump, double mpg, double fuel_left) {
+    return (distance_to_pump / mpg) <= fuel_left ? true : false;
+}
