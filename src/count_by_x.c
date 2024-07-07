abd7b92467930739a131bdb5afbf27ec8b971667
kfurkanbek <kfurkanbek@gmail.com>
2024-07-07 21:53:03 +0200

    Add a solution for Count by X problem

---
+++ src/count_by_x.c
@@ -0,0 +1,13 @@
+/**
+ * @brief a function with two arguments that will return an array of the first n
+multiples of x.
+ *
+ * @param x
+ * @param n
+ * @param result
+ */
+void count_by(unsigned x, unsigned n, unsigned result[n]) {
+    for (unsigned i = 0; i < n; i++) {
+        result[i] = (i + 1) * x;
+    }
+}
