076f16166bcca6fea78d67fbec98690e72a8f0fa
kfurkanbek <kfurkanbek@gmail.com>
2024-07-07 21:53:14 +0200

    Add a solution for Odd or Even problem

---
+++ src/odd_even.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+
+/**
+ * @brief Given a list of integers,
+determines whether the sum of its elements is odd or even.
+ *
+ * @param v
+ * @param sz
+ * @return const char*
+ */
+const char* odd_or_even(const int* v, size_t sz) {
+    int sum = 0;
+    for (size_t i = 0; i < sz; i++) {
+        sum += v[i];
+    }
+
+    return (sum % 2) ? "odd" : "even";
+}
